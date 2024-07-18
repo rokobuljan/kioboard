9764324d1995e8e8616eec348d039117f15a4562
rokobuljan <roko.cb@gmail.com>
2024-07-18 13:46:19 +0200

    imp: build

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,37 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import postcssNesting from "postcss-nesting";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const LIBRARYNAME = "kioboard";
+
 export default defineConfig({
-    base: './',
+    base: "./",
     build: {
-        minify: "terser",
+        minify: 'terser', // Or 'esbuild' (depending on your preference)
         lib: {
-            entry: path.resolve(__dirname, 'src/kioboard.js'),
-            name: 'kioboard',
-            fileName: 'kioboard',
-            formats: ['es'],
+            entry: path.resolve(__dirname, `src/${LIBRARYNAME}.js`),
+            name: "kioboard",
+            fileName: "kioboard",
+            // formats: ["es"],
             minify: true,
         },
         rollupOptions: {
             output: {
-                chunkFileNames: 'layouts/[name].js',
+                chunkFileNames: "layouts/[name].js",
+                assetFileNames: `${LIBRARYNAME}.[ext]`, // Prevent renaming kioboard.css to style.css
+                // entryFileNames: '[name].[format].js',
             }
         },
-        sourcemap: true,
+        sourcemap: false,
         emptyOutDir: true,
-    }
+    },
+    css: {
+        postcss: {
+            plugins: [postcssNesting()],
+        }
+    },
 });
