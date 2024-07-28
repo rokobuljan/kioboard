97531fae6fea4470194e3573d99e06943286cea9
rokobuljan <roko.cb@gmail.com>
2024-07-28 20:14:57 +0200

    imp: page fonts

--- src/page/index.js
+++ src/page/index.js
@@ -1,3 +1,6 @@
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/800.css';
 import "./hljs.css";
 import "./index.css";
 import Kioboard from "../kioboard.js";
